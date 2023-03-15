937908bcbba73dcba3674a2b1e57da644022935a
Quevin Bambasi <quevinbambasi@pm.me>
2023-03-15 16:41:17 -0400

    Initial Commit

---
+++ XcodeGHTest/XcodeGHTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGHTestApp.swift
+//  XcodeGHTest
+//
+//  Created by Quevin Bambasi on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGHTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
